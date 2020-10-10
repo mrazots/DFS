9889026ef3094f56a65beccaedd282d13467a913
urbeingwathced8 <tupikina_natasha@mail.ru>
2020-10-11 00:23:57 +0300

    Outline Client class

--- client/client.py
+++ client/client.py
@@ -1,1 +1,47 @@
-#To implement
+class Client:
+    def __init__(self):
+        pass
+
+    def connect(self, ip, port):
+        pass
+
+    def init_cluster(self):
+        pass
+
+    def mkdir(self, path):
+        pass
+
+    def lsdir(self, path):
+        pass
+
+    def cd(self, path):
+        pass
+
+    def rmdir(self, path):
+        pass
+
+    def touch(self, filepath):
+        pass
+
+    def upload(self, local_path, remote_path):
+        pass
+
+    def download(self, remote_path, local_path):
+        pass
+
+    def rm(self, path):
+        pass
+
+    def get_fileinfo(self, path):
+        pass
+
+    def cp(self, curr_path, dest_path):
+        pass
+
+    def mv(self, curr_path, dest_path):
+        pass
+
+
+
+if __name__ == '__main__':
+   pass
