987179089e826527e21eef2b779d284ce65a5945
mari1647iv <ima1647@outlook.com>
2020-10-11 06:04:58 +0300

    Define namenode code stucture

--- namenode/namenode.py
+++ namenode/namenode.py
@@ -1,1 +1,65 @@
-#To implement
+import socket
+import sys
+
+
+def initialize():
+    pass
+
+def check_nodes():
+    pass
+
+def send_message(socket, message):
+    pass
+
+
+def is_exists(filename):
+    pass
+
+def get_file_path(filename):
+    pass
+
+def mkdir(path, dirname):
+    pass
+
+def create_file(filename):
+    pass
+
+def read(filename):
+    pass
+
+def write(filename, str):
+    pass
+
+def delete_dir(dirname):
+    pass
+
+def delete_file(filename):
+    pass
+
+def ls():
+    pass
+
+def cd():
+    pass
+
+def cp(src, dest):
+    pass
+
+def mv(src, dest):
+    pass
+
+def make_query(query, is_return):
+    pass
+
+def close():
+    pass
+
+
+def backup(addr):
+    pass
+
+
+if __name__ == "__main__":
+    pass
+
+
